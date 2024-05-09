f4bcb57f60351484f6f8728127554ec3a341480c
Samurai <963513255@qq.com>
2024-05-09 14:13:59 +0800

    创建分支

--- lossfunc.py
+++ lossfunc.py
@@ -1,28 +1,19 @@
 import torch
 
 
-def msll(Fdb: torch.Tensor, mask: torch.Tensor):
-    """
-    计算(Batch_size,delta)的方向图副瓣电平(Batch_size,)
-    """
-    temp = Fdb.clone()
-
-    temp[mask] = float('-inf')
-    MSLL = temp.max(dim=1).values
-    return MSLL
-
-
 def total_loss(transformed_outputs: torch.Tensor, inputs: torch.Tensor, mask: torch.Tensor):
     """
     计算总损失值(主瓣MSE和MSLL)
     """
-    lossfun = torch.nn.MSELoss(reduction="mean")
+    mse_loss = torch.nn.MSELoss(reduction="mean")
     # (batch_size,)
-    loss_mll: torch.Tensor = lossfun(transformed_outputs[mask], inputs[mask])
+    loss_mll: torch.Tensor = mse_loss(transformed_outputs[mask], inputs[mask])
 
-    MSLL = msll(transformed_outputs, mask)
-    loss_msll: torch.Tensor = lossfun(MSLL, torch.zeros_like(MSLL)-50)
+    l1_loss = torch.nn.L1Loss()
+    diff = torch.relu(transformed_outputs[~mask]-inputs[~mask])
+    loss_sll: torch.Tensor = l1_loss(
+        torch.zeros_like(diff, device=diff.device), diff)
 
-    (w1, w2) = (0.5, 0.5)
+    (w1, w2) = (0.40, 0.60)
 
-    return w1*loss_mll+w2*loss_msll
+    return w1*loss_mll+w2*loss_sll
